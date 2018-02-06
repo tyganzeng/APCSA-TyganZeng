cf6a17890130fbbc298092b8fe69cfafdd39428e
tyganzeng <zengtygan@gmail.com>
2018-02-06 08:26:40 -0800

    Unit3
    Unit3

---
+++ Unit3/src/Lab03c.java
@@ -0,0 +1,28 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import java.util.Scanner;
+import static java.lang.System.*;
+import static java.lang.Math.*;
+
+public class Lab03c
+{
+	public static void main( String[] args )
+	{
+		Quadratic test = new Quadratic();
+		test.calcRoots();
+		test.print();
+		
+		Quadratic test2 = new Quadratic();
+		test2.calcRoots();
+		test2.print();
+		
+		Quadratic test3 = new Quadratic();
+		test3.calcRoots();
+		test3.print();
+   		
+	}
+}
---
+++ Unit3/src/Lab03d.java
@@ -0,0 +1,28 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import java.util.Scanner;
+import static java.lang.System.*;
+import static java.lang.Math.*;
+
+public class Lab03d
+{
+	public static void main( String[] args )
+	{
+		Distance test = new Distance();
+		test.calcDistance();
+		test.print();
+		
+		Distance test2 = new Distance();
+		test2.calcDistance();
+		test2.print();
+		
+		Distance test3 = new Distance();
+		test3.calcDistance();
+		test3.print();
+			
+	}
+}
