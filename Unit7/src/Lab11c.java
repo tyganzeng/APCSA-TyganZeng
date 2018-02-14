1eb238f0b5c27d06aa22034e1f9c08260e57cc2e
tyganzeng <zengtygan@gmail.com>
2018-02-14 09:54:29 -0800

    Unit7
    Added Unit7

---
+++ Unit7/src/Lab11c.java
@@ -0,0 +1,29 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class - 
+//Lab  -
+
+import static java.lang.System.*;
+
+public class Lab11c
+{
+   public static void main( String args[] )
+   {
+	   TriangleThree test = new TriangleThree(3,"A");
+	   System.out.println(test);
+	   test.setTriangle(7, "X");
+	   System.out.println(test);
+	   test.setTriangle(1, "R");
+	   System.out.println(test);
+	   test.setTriangle(5, "T");
+	   System.out.println(test);
+	   test.setTriangle(4, "W");
+	   System.out.println(test);
+	   
+	   
+
+
+
+	}
+}
---
+++ Unit7/src/TriangleThree.java
@@ -0,0 +1,70 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class TriangleThree
+{
+   private int size;
+   private String letter;
+
+	public TriangleThree()
+	{
+	}
+
+	public TriangleThree(int count, String let)
+	{
+		size = count;
+		letter = let;
+	}
+
+	public void setTriangle( int sz, String let )
+	{
+		size = sz;
+		letter = let;
+	}
+
+	public String getLetter()
+	{
+		return letter;
+	}
+
+	public String toString()
+	{
+		String output = "";
+	/*	for(int i = size; i > 0; i--){
+			for (int k = 1; k < i; k++){
+				System.out.print(" ");
+			}
+			for (int j = 1; j < size; j++){
+				System.out.print(letter);
+			}
+			System.out.println("\n");
+		}
+		*/
+		/*for (int i = 1; i <= size; i++){
+			for (int k = 0; k < -1 * i + size; k++){
+				System.out.print(" ");
+			}
+			for (int j = 0; j < i; j++){
+				System.out.print(letter);
+			}
+			System.out.println("\n");
+		}*/
+		
+		for (int i = 1; i <= size; i++){
+			for (int k = 0; k < -1 * i + size; k++){
+				output = output + " ";
+			}
+			for (int j = 0; j < i; j++){
+				output = output + letter;
+			}
+			output = output + "\n";
+		}
+		
+		return output;
+	}
+}
