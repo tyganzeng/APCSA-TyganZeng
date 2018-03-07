fecb1bf764460885daeeb0c154da68c86b6a7051
tyganzeng <zengtygan@gmail.com>
2018-03-07 08:53:12 -0800

    Unit10
    Added Unit10

---
+++ Unit10/src/Lab16d.java
@@ -0,0 +1,16 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class Lab16d
+{
+	public static void main( String args[] )
+	{
+		MadLib test = new MadLib("C:\\Users\\zengt9949\\Desktop\\APCSA-Tygan\\Unit10\\src\\story.dat");
+		
+	}
+}
