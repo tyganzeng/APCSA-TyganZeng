bdde932a452758ac07dce27df8d5f100083c480a
tyganzeng <zengtygan@gmail.com>
2018-03-12 09:12:35 -0700

    Unit11
    Unit11 complete

--- Unit11/src/Grades.java
+++ Unit11/src/Grades.java
@@ -5,13 +5,14 @@
 //Lab  -
 
 import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Scanner;
 import static java.lang.System.*;
 import static java.util.Arrays.*;
 
 public class Grades
 {
-	private double[] grades;
+	private ArrayList<Double> grades;
 	
 	public Grades()
 	{
@@ -25,27 +26,25 @@ public class Grades
 	
 	public void setGrades(String gradeList)
 	{
-		int size = gradeList.charAt(0);
-		grades = new double[size];
+		int size = gradeList.charAt(0) - '0';
+		grades = new ArrayList<Double>(size);
 		String list = gradeList.substring(4);
-		String[] numberStrs = list.split(",");
+		String[] numberStrs = list.split(" ");
 		for (int i = 0; i < size; i++){
-			grades[i] = Integer.parseInt(numberStrs[i]);
+			grades.add(Double.parseDouble(numberStrs[i]));
 		}
 	}
 	
 	public void setGrade(int spot, double grade)
 	{
-
-
-
+		grades.set(spot, grade);
 	}
 	
 	public double getSum()
 	{
 		double sum=0.0;
-		for (int i = 0; i < grades.length; i++){
-			sum += grades[i];
+		for (int i = 0; i < grades.size(); i++){
+			sum += grades.get(i);
 		}
 		return sum;
 	}
@@ -53,10 +52,11 @@ public class Grades
 	public double getLowGrade()
 	{
 		double low = Double.MAX_VALUE;
-
-
-
-
+		for(int i = 0; i < grades.size(); i++){
+			if(grades.get(i) < low){
+				low = grades.get(i);
+			}
+		}
 
 		return low;
 	}
@@ -64,27 +64,26 @@ public class Grades
 	public double getHighGrade()
 	{
 		double high = Double.MIN_VALUE;
-
-
-
-
+		for(int i = 0; i < grades.size(); i++){
+			if(grades.get(i) > high){
+				high = grades.get(i);
+			}
+		}
 
 		return high;
 	}
 	
 	public int getNumGrades()
 	{
-		return grades.length;
+		return grades.size();
 	}
 	
 	public String toString()
 	{
 		String output="";
-
-
-
-
-
+		for(int i = 0; i < grades.size(); i++){
+			output = output + grades.get(i) + " ";
+		}
 		return output;
 	}	
 }
